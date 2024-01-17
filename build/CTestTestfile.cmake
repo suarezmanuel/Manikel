d8ef29314e35f7c2de0af0f847325310da605260
suarezmanuel <manuelsuarez244@gmail.com>
2024-01-17 13:32:08 +0200

    first

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Projects/Manikel
+# Build directory: C:/Projects/Manikel/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(CalcTests "C:/Projects/Manikel/build/CalcTests.exe")
+set_tests_properties(CalcTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Projects/Manikel/CMakeLists.txt;34;add_test;C:/Projects/Manikel/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
