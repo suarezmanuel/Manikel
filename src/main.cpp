d6c842179b760a6f9365f98ad09b24d913b19bf9
suarezmanuel <manuelsuarez244@gmail.com>
2024-01-19 09:23:11 +0200

    more comprehensive main

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <functional>
 #include <string>
 #include <fstream>
 
 // change size, count inside func. x y z w ... where x denotes bloom size,
-// y: how many hashes for y hash, w: how many hashes for w hash...
-void getFirstParams (std::string in, int& size, std::vector<int>& hashes);
+// y: run y times the hash, w: run w times the hash...
+void getFirstParams (std::string in, int& size, std::hash<std::string> hash); // DOESNT NEED TEST
 // check for either: 1/2 URL, or x y z w ... (getFirstParams)
-bool checkInputFormatURLS (std::string in);
-bool checkInputFormatFirstParams (std::string in);
-bool is1URL (std::string in);
-void addToBlackList (std::string hash) {
+
+bool checkIs1URLOr2URL (std::string in); // TEST WRITTEN
+bool checkInputFormatFirstParams (std::string in); // TEST WRITTEN
+
+void addToBlackList (std::string hash) { // DOESNT NEED TEST
 
     std::fstream file;
     // we need both ios::out and ios::trunc
@@ -19,6 +21,7 @@ void addToBlackList (std::string hash) {
     // file << " i";
     file.close();
 }
+
 bool checkInBlackList (std::string in);
 bool checkFalsePos (std::string in);
 
@@ -27,30 +30,37 @@ int main () {
     std::string in;
     // size in bits
     int bloomSize;
-    std::vector<int> hashes;
-    std::vector<int> hashes;
-    
+    std::hash<std::string> hashes;
+    bool gotFirstParams = false;
     
     while (true) {
 
         // first line, bloom size, hashes
         std::getline(std::cin, in);
 
-        if (checkInputFormatURLS(in)) {
 
-            if (is1URL(in)) {
-                addToBlackList(in);
-            // must be 2URL
-            } else {
-                if(checkInBlackList(in)) { checkFalsePos(in) }
-            }
-        
-        // 123 2 2 2 2 2 2 2 2 
-        } else if (checkInputFormatFirstParams(in)) {
+        if (checkInputFormatFirstParams(in)) {
             getFirstParams(in, bloomSize, hashes);
+            gotFirstParams = true;
         }
+        
 
-        bloomSize = 0;
-        hashes.clear();
+        if (gotFirstParams && checkIs1URLOr2URL(in)) {
+
+            if (in[0] == '1') {
+                addToBlackList(in);
+
+            // must start with 2
+            } else {
+                bool check = checkInBlackList(in);
+                std::cout << check; 
+                
+                if (check) { 
+                    std::cout << " " << checkFalsePos(in) << std::endl;
+                } else {
+                    std::cout << std::endl;
+                }
+            }
+        }
     }
 }
