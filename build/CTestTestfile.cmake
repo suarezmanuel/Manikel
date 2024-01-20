de0e9edc1968170f7fc427556a7478e4ba541143
suarezmanuel <146821569+suarezmanuel@users.noreply.github.com>
2024-01-20 14:28:14 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Projects/Manikel
-# Build directory: C:/Projects/Manikel/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Projects/Manikel/build/InputTests[1]_include.cmake")
-subdirs("_deps/googletest-build")
